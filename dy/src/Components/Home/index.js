0444681f9948522bca9d253ba5eb008f875f475d
wuzang <351419742@qq.com>
2018-08-18 10:43:46 +0800

    jhp-999

--- dy/src/Components/Home/index.js
+++ dy/src/Components/Home/index.js
@@ -16,10 +16,12 @@ class Home extends Component{
 
 			datalist:[],
 			sideList:[],
-			hotList:[],
 			yzList:[],
 			liveList:[],
 			liveCount:0,
+			hotAllList:[],
+			hotList:[],
+            hotNum:0,
 
 			slideData: [],
             imgHeight: 210,
@@ -43,49 +45,115 @@ class Home extends Component{
         			</div>
         		</Sidebar>
 
-        				<WingBlank>
-        				    <Carousel
-        				      autoplay={true} 
-        		              infinite
-        		              autoplayInterval={1000}
-        		              dotStyle={{marginTop:'-20px'}}
-        		              dotActiveStyle={{marginTop:'-20px'}}
-        				    >
-        				      {this.state.slideData.map(item => (
-        				        <a
-        				          key={item.rid}
-        		                  onClick={this.slideClick.bind(this,item)}
-        				          style={{ display: 'inline-block', width: '100%' }}
-        				        >
-        				          <img
-        				            src={`${item.pic}`}
-        				            alt=""
-        				            style={{ width: '100%', verticalAlign: 'top',height:"240px" }}
-        				            onLoad={() => {
-        				              window.dispatchEvent(new Event('resize'));
-        				              this.setState({ imgHeight: 'auto' });
-        				            }}
-        				          />
-        				        </a>
-        				      ))}
-        				    </Carousel>
-        				</WingBlank>
+        		<WingBlank>
+        		    <Carousel
+        		      autoplay={true} 
+        		            infinite
+        		            autoplayInterval={1000}
+        		            dotStyle={{marginTop:'-20px'}}
+        		            dotActiveStyle={{marginTop:'-20px'}}
+        		    >
+        		      {this.state.slideData.map(item => (
+        		        <a
+        		          key={item.rid}
+        		                onClick={this.slideClick.bind(this,item)}
+        		          style={{ display: 'inline-block', width: '100%' }}
+        		        >
+        		          <img
+        		            src={`${item.pic}`}
+        		            alt=""
+        		            style={{ width: '100%', verticalAlign: 'top',height:"240px" }}
+        		            onLoad={() => {
+        		              window.dispatchEvent(new Event('resize'));
+        		              this.setState({ imgHeight: 'auto' });
+        		            }}
+        		          />
+        		        </a>
+        		      ))}
+        		    </Carousel>
+        		</WingBlank>
 
         		<header>
-        			<ol>
-        			{
-        				this.state.hotList.map(item=>
-        				   <li key={item.rid}>
-        				   	<div className="headerRoomshow">
-        				   		<span className="headericoTop iconfont icon-zuire"> {item.hn} &nbsp;</span>
-        				   		<img src={item.roomSrc} alt={item.nickname}/>
-        				   		<span className="headericoBottom iconfont icon-wo"> {item.nickname}</span>
-        				   	</div>
-        				   	<p>{item.roomName}</p>
-        				   </li>
-        				)
-        			}
-        			</ol>
+        			<div className="headerStyle headerHot">
+        				<h3>
+        					<div>
+        						<span className="hot iconfont icon-zuire"></span>
+        						<b>最热</b>
+        					</div>
+        					<p onClick={this.changeClick.bind(this)}>
+        						<b>换一换</b>
+        						<span className="change iconfont icon-refresh"></span>
+        					</p>
+        				</h3>
+        				<ol>
+        					{
+        					this.state.hotList.map(item=>
+        				  	 <li key={item.rid} onClick={this.homelistClick.bind(this,item)}>
+        				   		<div className="headerRoomshow">
+        				   			<span className="headericoTop iconfont icon-zuire"> {item.hn} &nbsp;</span>
+        				   			<img src={item.roomSrc} alt={item.nickname}/>
+        				   			<span className="headericoBottom iconfont icon-wo"> {item.nickname}</span>
+        				   		</div>
+        				   		<p>{item.roomName}</p>
+        				   	</li>
+        						)
+        					}
+        				</ol>
+        			</div>
+
+        			<div className="headerStyle headerLive">
+        				<h3>
+        					<div >
+        						<span className="hot iconfont icon-zhibo"></span>
+        						<b> 正在直播</b>
+        					</div>
+        					<p   onClick={this.qbClick.bind(this)}>
+        						<b>当前<i>{this.state.liveCount}</i>个直播</b>
+        						<span>></span>
+        					</p>
+        				</h3>
+        				<ol>
+        					{
+        					this.state.liveList.map(item=>
+        				  	 <li key={item.rid} onClick={this.homelistClick.bind(this,item)}>
+        				   		<div className="headerRoomshow">
+        				   			<span className="headericoTop iconfont icon-zuire"> {item.hn} &nbsp;</span>
+        				   			<img src={item.roomSrc} alt={item.nickname}/>
+        				   			<span className="headericoBottom iconfont icon-wo"> {item.nickname}</span>
+        				   		</div>
+        				   		<p>{item.roomName}</p>
+        				   	</li>
+        						)
+        					}
+        				</ol>
+        			</div>
+
+        			<div className="headerStyle headerYz">
+        				<h3>
+        					<div>
+        						<span className="yz iconfont icon-fenlei-yanzhi"></span>
+        						<b> 颜值</b>
+        					</div>
+        					<p  onClick={this.yzClick.bind(this)}>
+        						<b>更多</b>
+        						<span>></span>
+        					</p>
+        				</h3>
+        				<ol>
+        					{
+        					this.state.yzList.map(item=>
+        				  	 <li key={item.rid} onClick={this.homelistClick.bind(this,item)}>
+        				   		<div className="headerRoomshow">
+        				   			<span className="yzIco iconfont icon-zuire"> {item.hn} &nbsp;</span>
+        				   			<img className="meinv" src={item.verticalSrc} alt={item.nickname}/>
+        				   		</div>
+        				   		<p className="headericoBottom iconfont icon-wo"> {item.nickname}</p>
+        				   		<b className="iconfont icon-didian"> {item.liveCity}</b>
+        				   	</li>
+        						)
+        					}
+        				</ol>
+        			</div>
         		</header>
 
         		<main>
@@ -130,11 +198,7 @@ class Home extends Component{
 					<p>湖北省武汉市东湖开发区光谷软件园F4栋8楼</p>
 					<p>版权所有 © www.douyu.com 鄂ICP备15011961号-1</p>
 				</footer>
-
-        		
-        		
-			</section>
-			
+			</section>			
        	)
 	}
 
@@ -144,7 +208,6 @@ class Home extends Component{
                 slideData:res[0].data.slideList
             })
 				// console.log("全部",res[0])
-                console.log(res[0].data.slideList)
 
 		}).catch(error=>{
 			console.log(error)		
@@ -156,7 +219,8 @@ class Home extends Component{
 			hotList:res.data.data.hotList[0].list,
 			yzList:res.data.data.yzList,
 			liveList:res.data.data.liveList,
-			liveCount:res.data.data.liveCount
+			liveCount:res.data.data.liveCount,
+			hotAllList:res.data.data.hotList
 			})
 		}).catch((err)=>{
 			console.log('error')
@@ -175,6 +239,29 @@ class Home extends Component{
 		this.props.detailPage(item);
 		localStorage.setItem('detailPage',JSON.stringify(item));
 		this.props.history.push(`/detail/${item.rid}/`);
+	}
+
+	//换一换点击事件
+	changeClick(){
+		var num = (this.state.hotNum+1)%3;
+		this.setState({
+			hotNum:num,
+			hotList:this.state.hotAllList[num].list
+		})
+	}
+
+
+	//颜值点击事件
+	yzClick(){
+		localStorage.setItem('listPage',JSON.stringify({'tabName':'颜值'}));
+		this.props.history.push(`/list/yz/`);
+	}
+
+	//全部点击事件
+	qbClick(){
+		
+		localStorage.setItem('listPage',JSON.stringify({'tabName':'全部'}));
+		this.props.history.push(`/list/room/`);
 	}
 
 	//更多点击事件
